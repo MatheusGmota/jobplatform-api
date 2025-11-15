8a38c0f680fc8d98b3243c6db29457ab0d2dacc1
MatheusGmota <livematgreg@gmail.com>
2025-11-15 17:54:32 -0300

    Inicializando projeto

---
+++ src/main/java/br/com/api/workTree/WorkTreeApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.workTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/workTree/WorkTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.workTree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
