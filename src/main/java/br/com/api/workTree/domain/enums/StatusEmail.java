bd88f009f9c3e07cc870c4abbd0f778785142c1e
MatheusGmota <livematgreg@gmail.com>
2025-11-20 21:28:55 -0300

    Implementado serviço de mensageria e criação de texto para corpo do email

---
+++ src/main/java/br/com/api/workTree/domain/enums/StatusEmail.java
@@ -0,0 +1,7 @@
+package br.com.api.workTree.domain.enums;
+
+public enum StatusEmail {
+    PROCESSING,
+    SENT,
+    ERROR;
+}
---
+++ src/main/java/br/com/api/workTree/services/EmailSenderService.java
@@ -0,0 +1,34 @@
+package br.com.api.workTree.services;
+
+import br.com.api.workTree.domain.enums.StatusEmail;
+import br.com.api.workTree.domain.model.EmailModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EmailSenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Transactional
+    public void sendEmail(EmailModel emailModel) {
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("livematgreg@gmail.com");
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            mailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        }
+//        System.out.println("Email enviado com sucesso");
+    }
+}
